674d445559a67eb06e629bc195a9fb8d9e5af352
Sheerforce <Sheerforce@users.noreply.github.com>
2015-06-18 15:59:04 -0400

    Create Servo-Set-Middle.ino

---
+++ Servo-Set-Middle.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+#define SERVO_DATA_PIN 3
+
+Servo sonicservo;
+
+void setup(){
+    sonicservo.attach(SERVO_DATA_PIN)
+    sonicservo.write(90);
+}
+
+void loop(){
+    Serial.println("Callibration finished!")
+}
